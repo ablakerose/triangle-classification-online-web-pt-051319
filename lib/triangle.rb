ff9605d7d249419daa4cae3396835e85a125e40e
Blake Rose <bdecktor@gmail.com>
2019-06-18 22:45:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -31,10 +31,11 @@ class Triangle
       end
 
     end
+    class TriangleError < StandardError
+      def message
+        puts "Not a triangle."
+    end
   end
 
-  class TriangleError < StandardError
-    def message
-      puts "Not a triangle."
-  end
+  
 end
